a9b0d1d5d087ef2052e9f4197fd6982f10ade1dd
Huynh Duc Dung <dunghd.it@gmail.com>
2022-11-13 00:28:55 +0800

    refactor(web): move out the hello connect demo

---
+++ apps/web/components/HelloConnectApp.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { Button } from "ui";
+
+import {
+  createConnectTransport,
+  createPromiseClient,
+} from "@bufbuild/connect-web";
+
+// Import service definition that you want to connect to.
+import { GreetService } from "hello-go/types/greet/v1/greet_connectweb";
+
+// The transport defines what type of endpoint we're hitting.
+// In our example we'll be communicating with a Connect endpoint.
+const transport = createConnectTransport({
+  baseUrl: "http://localhost:8080", // this is calling connect-go server
+});
+
+// Here we make the client itself, combining the service
+// definition with the transport.
+export const client = createPromiseClient(GreetService, transport);
+
+export default function HelloConnectApp() {
+  const [inputValue, setInputValue] = useState("");
+  return (
+    <>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          try {
+            const result = await client.greet({
+              name: inputValue,
+            });
+            console.log(result);
+            alert(JSON.stringify(result))
+          } catch (error) {
+            alert(JSON.stringify(error));
+          }
+        }}
+      >
+        <input
+          value={inputValue}
+          required
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Button name="Send" type="submit" />
+      </form>
+    </>
+  );
+}
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,55 +1,14 @@
-import { useState } from "react";
-import { Button } from "ui";
-import {
-  createConnectTransport,
-  createPromiseClient,
-} from "@bufbuild/connect-web";
-
-// Import service definition that you want to connect to.
-import { GreetService } from "hello-go/types/greet/v1/greet_connectweb";
-
-// The transport defines what type of endpoint we're hitting.
-// In our example we'll be communicating with a Connect endpoint.
-const transport = createConnectTransport({
-  baseUrl: "http://localhost:8080", // this is calling connect-go server
-});
-
-// Here we make the client itself, combining the service
-// definition with the transport.
-const client = createPromiseClient(GreetService, transport);
-
-function HelloApp() {
-  const [inputValue, setInputValue] = useState("");
-  return (
-    <>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          try {
-            const result = await client.greet({
-              name: inputValue,
-            });
-            console.log(result);
-          } catch (error) {
-            alert(JSON.stringify(error));
-          }
-        }}
-      >
-        <input
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <Button name="send" type="submit" />
-      </form>
-    </>
-  );
-}
+import Head from "next/head";
+import HelloConnectApp from "../components/HelloConnectApp";
 
 export default function Web() {
   return (
     <div>
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
+      </Head>
       <h1>Connect Web</h1>
-      <HelloApp />
+      <HelloConnectApp />
     </div>
   );
 }
